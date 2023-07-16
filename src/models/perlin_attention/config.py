update refactor, numerical correct

src/models/perlin_attention/config.py
@@ -1,11 +1,13 @@
+import json
+import math
+from dataclasses import asdict, dataclass
+from typing import Dict, List, Optional, Tuple
+
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
-import math
-from dataclasses import dataclass, asdict
+
 from ..hf_bert import BertConfig
-from typing import Optional, Tuple, List, Dict
-import json
 
 @dataclass
 class PerlinAttentionConfig:
@@ -21,10 +23,13 @@ class PerlinAttentionConfig:
     attention_predictor_comp_patch_size: int = 16
     attention_predictor_comp_patch_count: int = 16
     layerwise: bool = False
-    lora_r: int = 32
+    lora_r: int = 16
     lora_enabed: bool = False
     lora_in_approx_enabled: bool = False
     partial_attention_scaler: bool = True
+    out_add_performer_context: bool = False
+    v_eye_length: int = 128
+    out_norm: bool = False
     
     def to_json(self):
         return asdict(self)

src/models/perlin_attention/self_attention.py
@@ -1,14 +1,21 @@
+import math
+import warnings
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Tuple
+
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
-import math
-from dataclasses import dataclass
+
+from ..common.lora import (
+    LoraLinear, 
+    lora_forward, 
+    lora_forward_linear,
+    lora_forward_lora
+)
 from ..hf_bert import BertConfig
-from typing import Optional, Tuple, List, Dict
-from .config import PerlinAttentionConfig, get_default_config
 from .attention import PerlinAttention, PerlinAttentionOutput
-import warnings
-from ..common.lora import LoraLinear, lora_forward, lora_forward_linear, lora_forward_lora
+from .config import PerlinAttentionConfig, get_default_config
 
 default_lazy = lambda x, d: d() if x is None else x
 