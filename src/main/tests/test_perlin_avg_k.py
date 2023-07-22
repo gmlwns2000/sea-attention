97c87c4e6a30f16e82e410d75973280ecdcd18b3
AinL <gmlwns5176@gmail.com>
2023-07-22 15:59:45 +0900

    fix bug

--- src/main/tests/test_perlin_avg_k.py
+++ src/main/tests/test_perlin_avg_k.py
@@ -18,31 +18,36 @@ def main(
     )
     trainer.load(path=checkpoint_path)
     
+    trainer.base_model.eval()
+    trainer.model.eval()
+    
     acc_sum = 0
     acc_count = 0
     k_sum = 0
     k_count = 0
-    for batch in tqdm.tqdm(trainer.valid_loader, dynamic_ncols=True):
-        batch = batch_to(batch, trainer.device)
-        with torch.no_grad():
-            batch['output_attentions'] = True
-            batch['output_hidden_states'] = True
-            trainer.base_model(**batch)
-            batch['teacher'] = trainer.base_model
-            output = trainer.model(**batch)
-            acc_sum += (torch.argmax(output.logits, dim=-1) == batch['labels']).float().sum().item()
-            acc_count += len(batch['labels'])
-        
-        for layer in trainer.model.bert.encoder.layer:
-            layer = layer # type: perlin_bert.BertLayer
-            N, H, T, T_1 = layer.attention.self.last_perlin_partial_probs.shape
-            assert T == T_1
-            elem_alive = (layer.attention.self.last_perlin_partial_probs.abs() > 1e-8).float().view(N, -1).sum(dim=-1)
-            token_length = batch['attention_mask'].sum(-1)
-            k = (elem_alive / token_length / H).sum()
-            k_sum += k.item()
-            k_count += N
-        print(k_sum / k_count, acc_sum / acc_count)
+    with tqdm.tqdm(trainer.valid_loader, dynamic_ncols=True) as pbar:
+        for batch in pbar:
+            batch = batch_to(batch, trainer.device)
+            with torch.no_grad(), torch.autocast('cuda', torch.float16):
+                batch['output_attentions'] = True
+                batch['output_hidden_states'] = True
+                trainer.base_model(**batch)
+                batch['teacher'] = trainer.base_model
+                output = trainer.model(**batch)
+                acc_sum += (torch.argmax(output.logits, dim=-1) == batch['labels']).float().sum().item()
+                acc_count += len(batch['labels'])
+            
+            for layer in trainer.model.bert.encoder.layer:
+                layer = layer # type: perlin_bert.BertLayer
+                N, H, T, T_1 = layer.attention.self.last_perlin_partial_probs.shape
+                assert T == T_1
+                elem_alive = (layer.attention.self.last_perlin_partial_probs.abs() > 1e-8).float().view(N, -1).sum(dim=-1)
+                token_length = batch['attention_mask'].sum(-1)
+                k = (elem_alive / token_length / H).sum()
+                k_sum += k.item()
+                k_count += N
+            pbar.set_description(f'k:{k_sum/k_count:.2f} acc:{acc_sum/acc_count:.4f}')
+    print(f'dataset average k = {k_sum / k_count:.3f}, dataset accuracy = {acc_sum / acc_count * 100:.2f} %')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
