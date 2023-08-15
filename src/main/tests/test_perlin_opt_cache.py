317834a7c6326baecb37ef7c360c648d03539108
AinL <gmlwns5176@gmail.com>
2023-08-15 17:45:28 +0900

    time to work generation!

--- src/main/tests/test_perlin_opt_cache.py
+++ src/main/tests/test_perlin_opt_cache.py
@@ -74,6 +74,7 @@ def main():
     
     # trainer, model, tokenizer = init(skip_init_loaders=True, checkpoint_path='./saves/trainer/opt_trainer/opt-125m_wikitext2_kf1_lw0_perlin_k64_full_copy/checkpoint.pth')
     trainer, model, tokenizer = init(skip_init_loaders=True)
+    model.eval()
     
     input_ids = tokenizer(
         "Famitsu enjoyed the story , and were particularly pleased with the improvements to gameplay . Japanese gaming site Game Watch <unk> , despite negatively noting its pacing and elements recycled from previous games , was generally positive about its story and characters , and found its gameplay entertaining despite off @-@ putting difficulty spikes . <unk> writer <unk> <unk> , in a Play Test article based on the game 's <unk> demo , felt that Valkyria Chronicles III provided a profound feeling of closure for the Valkyria Chronicles series . He praised its gameplay despite annoying limitations to aspects such as special abilities , and positively noted its shift in story to a tone similar to the first game . PlayStation Official Magazine - UK praised the story 's <unk> of Gallia 's moral standing , art style , and most points about its gameplay , positively noting the latter for both its continued quality and the tweaks to balance and content . Its one major criticism were multiple difficulty spikes , something that had affected the previous games . Heath Hindman of gaming website PlayStation <unk> praised the addition of non @-@ linear elements and improvements or removal of mechanics from Valkyria Chronicles II in addition to praising the returning gameplay style of previous games . He also positively noted the story 's serious tone . Points criticized in the review were recycled elements , awkward cutscenes that seemed to include all characters in a scene for no good reason , pacing issues , and occasional problems with the game 's AI ",
@@ -95,22 +96,29 @@ def main():
     
     dense_output = torch.argmax(output.logits, dim=-1)[0].cpu().numpy()
     dense_text = tokenizer.batch_decode(dense_output.reshape(1, -1), skip_special_tokens=True, clean_up_tokenization_spaces=True)[0]
-    # print(dense_output, dense_text)
+    print(dense_output, dense_text)
     
     # sample with cache
+    bench.reset_measures()
+    bench.reset_trace()
     past_key_values = None
     output_ids = []
     perlin_attention.get_default_config().use_cache = use_cache
+    # for module in model.modules():
+    #     if hasattr(module, 'benchmarking'):
+    #         module.benchmarking = True
+    #         print(type(module))
     buffers = {}
     for i in tqdm.tqdm(range(input_ids.shape[-1])):
         if use_cache:
             ids_slice = input_ids[:, i:i+1]
             with torch.no_grad():
-                output = model(
-                    input_ids=ids_slice,
-                    past_key_values=past_key_values,
-                    use_cache=True,
-                )
+                with get_bench().region("sample"):
+                    output = model(
+                        input_ids=ids_slice,
+                        past_key_values=past_key_values,
+                        use_cache=True,
+                    )
             past_key_values = output.past_key_values
             token_id = torch.argmax(output.logits, dim=-1).item()
             
@@ -171,10 +179,12 @@ def main():
             
             bench.reset_temp_buffers()
         output_ids.append(token_id)
-        
+    
+    print(bench.format_tracetree())
+    
     cached_output = np.array(output_ids)
     cached_text = tokenizer.batch_decode(cached_output.reshape(1, -1), skip_special_tokens=True, clean_up_tokenization_spaces=True)[0]
-    # print(cached_output, cached_text)
+    print(cached_output, cached_text)
     print('accuracy', ((cached_output == dense_output) * 1.0).mean())
     
     # print('truth', strify(buffers_truth))
