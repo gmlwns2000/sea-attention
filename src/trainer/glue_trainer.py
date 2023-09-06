da5845f1783117ad5ea759909e18a63db134ac13
jk020218 <jk020218@gmail.com>
2023-09-06 22:05:56 +0900

    update path name

--- src/trainer/glue_trainer.py
+++ src/trainer/glue_trainer.py
@@ -360,7 +360,7 @@ class Trainer:
 
     def checkpoint_path(self):
         os.makedirs(f'./saves/trainer/bert_glue_trainer/{self.exp_name}/', exist_ok=True)
-        path = f'./saves/trainer/bert_glue_trainer/{self.exp_name}/checkpoint.pth'
+        path = f'./saves/trainer/bert_glue_trainer/{self.exp_name}/checkpoint_false.pth'
         return path
     
     def save(self):
