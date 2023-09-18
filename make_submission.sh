a41c939151b6472df8309ef36db306dc59b50734
AinL <gmlwns5176@gmail.com>
2023-09-18 19:49:08 +0900

    update submission

--- make_submission.sh
+++ make_submission.sh
@@ -1,8 +1,24 @@
 mkdir -p submission
+
+# checkout from current git branch
 git checkout-index -a -f --prefix=./submission/
+# remove private sensitive data
 rm submission/build_docker.sh
 rm submission/src/main/export_wandb.py
 rm submission/src/poc/neko/test_causal_conv.ipynb
+
+# copy plots and evaluation results, removes OPT visualization due to size limit
 mkdir -p submission/plots
-cp -i -r plots/* submission/plots
+cp -rf plots/* submission/plots
+rm -rf submission/plots/visualize_opt
+rm -rf submission/plots/visualize_opt_backup
+rm -rf submission/plots/main/figure_visualization_opt/wikitext2_0
+rm -rf submission/plots/main/figure_visualization_opt/wikitext2_1
+rm -rf submission/plots/main/figure_visualization_opt/wikitext2_2
+rm -rf submission/plots/main/figure_visualization_opt/wikitext2_3
+
+# zip the submission
+timestamp=$(date +'%m_%d_%Y')
+zip -r "submission_${timestamp}.zip" submission/*
+
 echo done
