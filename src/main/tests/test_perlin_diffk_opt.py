71895fceb2b690e330aa165665cf6f823c9259f7
jk020218 <jk020218@gmail.com>
2025-01-06 01:59:59 +0900

    minor update for diffk

--- src/main/tests/test_perlin_diffk_opt.py
+++ src/main/tests/test_perlin_diffk_opt.py
@@ -25,7 +25,7 @@ def main(
     for i in range(31):
         k_test.append(16+8*i)
     print(f'k_test : {k_test}')    
-    for trained_k in [64]:# 32, 64, 
+    for trained_k in [32, 64, 128]:
         warnings.warn(f"trained k {trained_k}")
         kwargs['perlin_k']=trained_k
         print(kwargs)
@@ -44,12 +44,6 @@ def main(
             if hasattr(module, 'benchmarking'):
                 module.benchmarking = False
         
-        # valid_loader = get_dataloader(
-        #     subset='valid', 
-        #     tokenizer=trainer.tokenizer, 
-        #     batch_size=1, 
-        #     max_length=32000,
-        # )
         perlin_attention.get_default_config().k = trained_k
         pretrained_ppl = trainer.evaluate()
         print(f'pretrained_k:{trained_k}, pretrained_ppl:{pretrained_ppl}')
@@ -58,22 +52,6 @@ def main(
         for k in k_test:
             perlin_attention.get_default_config().k = k
             score = trainer.evaluate()
-            # acc_sum = 0
-            # acc_count = 0
-            # with tqdm.tqdm(valid_loader, dynamic_ncols=True) as pbar:
-            #     for batch in pbar:
-            #         batch = batch_to(batch, trainer.device)
-            #         with torch.no_grad(), torch.autocast('cuda', torch.float32):
-            #             batch['output_attentions'] = True
-            #             batch['output_hidden_states'] = True
-            #             trainer.base_model(**batch)
-            #             batch['teacher'] = trainer.base_model
-            #             output = trainer.model(**batch)
-            #             acc_sum += (torch.argmax(output.logits, dim=-1) == batch['labels']).float().sum().item()
-            #             acc_count += len(batch['labels'])
-                    
-            #         pbar.set_description(f'k:{k}, acc:{acc_sum/acc_count:.4f}')
-            # final_acc = acc_sum/acc_count*100
             
             print(f'k:{k}, ppl:{score}')
             k_acc_bucket[k]=score
